de9cbf8d6457ba0dd4ef37dde1934793408310a8
haojt <jiangtao.hao@supwisdom.com>
2020-11-06 11:33:52 +0800

    wait

--- src/main/java/com/lhc/listIterator/MyList.java
+++ src/main/java/com/lhc/listIterator/MyList.java
@@ -1,5 +1,8 @@
 package com.lhc.listIterator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Description:
  * User: jt.hao
@@ -7,4 +10,18 @@ package com.lhc.listIterator;
  * Time: 16:45
  */
 public class MyList {
+
+    public static void main(String[] args){
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+        for (String s : list) {
+            if("a".equals(s)){
+                //list.remove(s);
+                list.add("a++");
+            }
+        }
+    }
 }
